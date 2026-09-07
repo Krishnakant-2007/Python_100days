d43760b60aeabbe6a8b857829451941d3d652340
krishnaiitb2026-cmd <krishnaiitb.2026@gmail.com>
2026-09-07 10:18:42 +0530

    ZERO TO HERO

--- Day-8.py
+++ Day-8.py
@@ -5,8 +5,7 @@ a = int(input("Enter First Number :"))
 b = int(input("Enter Second Number :"))
 c = input("Select What you want to do; \"+\" , \"-\" , \"*\" , \"/\" , \"**\" , \"//\" , \"%\" :")
 
-if c!= "+" or "-" or "*" or "/" or "**" or "//" or "%":
-    print("\n\nBakchodi Kyu kr rha h......\n")
+
 if c == "+":
     print(f"{a}+{b} = {a+b}")
 if c == "-":
@@ -21,3 +20,5 @@ if c == "//":
     print(f"{a}//{b} = {a//b}")
 if c == "%":
     print(f"{a}%{b} = {a%b}")
+else:
+    print("\nbachodi mat kar".title())
